ee94857d6fccb89cb63ea3352ce43a603451ad55
Antoine De Cock <34279257+spartoine118@users.noreply.github.com>
2024-11-14 11:19:05 +0700

    revert changes use multiple buckets

--- infrastructure/pre-prod/main.tf
+++ infrastructure/pre-prod/main.tf
@@ -17,15 +17,9 @@ module "storage" {
   location = var.REGION
 }
 
-resource "google_storage_bucket_object" "content_folder" {
-  name    = "${var.ENV}/"
-  content = "nothing"
-  bucket  = var.storage_name
-}
-
 resource "local_file" "default" {
   file_permission = "0644"
-  filename        = "${path.module}/${var.ENV}/backend.tf"
+  filename        = "${path.module}/backend.tf"
 
   # You can store the template in a file and use the templatefile function for
   # more modularity, if you prefer, instead of storing the template inline as
@@ -39,19 +33,19 @@ resource "local_file" "default" {
   EOT
 }
 
-# module "service" {
-#   source          = "../modules/google-cloud-service"
-#   image_name      = var.DOCKER_IMAGE
-#   name            = "cloud-function-pre-prod"
-#   location        = var.REGION
-#   service_account = data.google_secret_manager_secret_version.SA_EMAIL.secret_data
-#   container_env   = var.container_env
-# }
+module "service" {
+  source          = "../modules/google-cloud-service"
+  image_name      = var.DOCKER_IMAGE
+  name            = "cloud-function-pre-prod"
+  location        = var.REGION
+  service_account = data.google_secret_manager_secret_version.SA_EMAIL.secret_data
+  container_env   = var.container_env
+}
 
-# module "scheduler" {
-#   source                = "../modules/google-cloud-scheduler"
-#   name                  = "cloud-service-daily-scheduler-pre-prod"
-#   uri                   = module.service.service_uri
-#   service_account_email = data.google_secret_manager_secret_version.SA_EMAIL.secret_data
-#   audience              = module.service.service_uri
-# }
+module "scheduler" {
+  source                = "../modules/google-cloud-scheduler"
+  name                  = "cloud-service-daily-scheduler-pre-prod"
+  uri                   = module.service.service_uri
+  service_account_email = data.google_secret_manager_secret_version.SA_EMAIL.secret_data
+  audience              = module.service.service_uri
+}
